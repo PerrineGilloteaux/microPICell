e74b499e2b13c3a57fc28c4a3151462e9daca903
Perrine Paul-Gilloteaux <perrine.paul-gilloteaux@univ-nantes.fr>
2019-07-15 13:43:18 +0200

    corrected scrip and classifier
    Scripts and classifier used for plaque detection

--- QuPathScript/AortaPlaqquesegmentation/scripts/Analyse.groovy
+++ QuPathScript/AortaPlaqquesegmentation/scripts/Analyse.groovy
@@ -5,5 +5,5 @@ runPlugin('qupath.lib.algorithms.TilerPlugin', '{"tileSizeMicrons": 15.0,  "trim
 selectDetections();
 runPlugin('qupath.lib.algorithms.IntensityFeaturesPlugin', '{"pixelSizeMicrons": 2.0,  "region": "Square tiles",  "tileSizeMicrons": 15.0,  "colorOD": true,  "colorStain1": true,  "colorStain2": true,  "colorStain3": true,  "colorRed": true,  "colorGreen": false,  "colorBlue": true,  "colorHue": false,  "colorSaturation": true,  "colorBrightness": true,  "doMean": true,  "doStdDev": true,  "doMinMax": false,  "doMedian": false,  "doHaralick": true,  "haralickDistance": 1,  "haralickBins": 32}');
 //Apply the precomputed classifier
-runClassifier('D:\\MarineCoue\\testinguPath\\AnalyseAorte\\classifiers\\FirstClassifierPlaqueAorte.qpclassifier');
+runClassifier('C:\\Users\\Axio Imager M2\\Documents\\MarineCoue\\AorteEntiere\\classifiers\\NewclassifioerDoneWithMarine.qpclassifier');
 
